d627d2119d521914e443a1a8eaae7eadf2ea8836
A <alyssa@kvasl.com>
2024-03-22 15:42:36 +1300

    Stock model inital made

---
+++ sink/backend/models/stockModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const stockSchema = new Schema ({
+    name: { 
+        type: String,
+        required: True
+    },
+    date: { 
+        type: String, //date not type: Date as string allows more flexibility
+    },
+    dimensions: {
+        type: String,
+    },
+    artist: {
+        type: String,
+        default: 'Unknown'
+    },
+    medium: [{
+        type: String,
+    }],
+    description: {
+        type: String
+    },
+    images: [{
+        type: String,
+    }]
+
+    
+    
+})
