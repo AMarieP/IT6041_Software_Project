e90f91315d203ca54c88b3b4b2252fe71f85f8b4
A <alyssa@kvasl.com>
2024-03-26 11:24:49 +1300

    Imported controller, updated routes

--- sink/backend/routes/stock.js
+++ sink/backend/routes/stock.js
@@ -1,30 +1,28 @@
 const express = require('express')
+const {
+    createStock,
+    getAllStock,
+    getOneStockByID,
+    updateStock,
+    deleteStock
+} = require('../controllers/stock')
+
 const router = express.Router()
 
 
 //GET All Stock 
-router.get('/', (req, res) => {
-    res.json({mssg: "GET all art stock"})
-})
+router.get('/', getAllStock)
 
 //GET One Stock By ID
-router.get('/:id', (req, res) => {
-    res.json({mssg: "GET all art stock"})
-})
+router.get('/:id', getOneStockByID)
 
 //POST New Stock
-router.post('/', (req, res) => {
-    res.json({mssg: "Create New Stock"})
-})
-
-//DELETE One Stock
-router.delete('/', (req, res) => {
-    res.json({mssg: "Delete One Stock"})
-})
+router.post('/', createStock)
 
 //PATCH Update One Stock
-router.patch('/', (req, res) => {
-    res.json({mssg: "Update One Stock"})
-})
+router.patch('/:id', updateStock)
+
+//DELETE One Stock
+router.delete('/:id', deleteStock)
 
 module.exports = router
