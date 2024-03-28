ea563768253d6b99c6e8b0d5a75affda6b6726a6
A <alyssa@kvasl.com>
2024-03-28 17:58:56 +1300

    inital code for box

--- componentsCreate/components/src/components/boxWithDropshadow.js
+++ componentsCreate/components/src/components/boxWithDropshadow.js
@@ -1,9 +1,21 @@
+import { withTheme } from '@rneui/themed'
 import React from 'react'
 
-function boxWithDropshadow() {
+function BoxWithDropshadow({children}) {
   return (
-    <div>boxWithDropshadow</div>
+    <div style={styles.container}>
+        {children}
+    </div>
   )
 }
 
-export default boxWithDropshadow
+
+const styles = {
+    container:{
+        backgroundColor: 'white',
+        boxShadow: '4px 4px 5px',
+        border: '1px solid black;',
+        padding: '5%'
+    }
+}
+export default BoxWithDropshadow
