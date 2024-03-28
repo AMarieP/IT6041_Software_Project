ef5b953e1434272bcdb79bb58408687b5e6790a0
A <alyssa@kvasl.com>
2024-03-28 19:25:11 +1300

    Prelim slider toggle

--- componentsCreate/components/src/components/sliderToggle.js
+++ componentsCreate/components/src/components/sliderToggle.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import {React, useState} from 'react'
 
-function sliderToggle() {
+/*
+How it works: usestate to toggle on and off
+useState changes styling on checkbox
+
+using CSS tutorials incl this on
+https://www.w3schools.com/howto/howto_css_switch.asp
+https://alvarotrigo.com/blog/toggle-switch-css/
+TODO: pretty much all of it hahahah
+-reduce if/else to the shorthand for easier reading
+-do all of the styling thus far
+-probably sort the animation as well 
+-finsih the todo
+
+ */
+
+
+function sliderToggle({name, leftToggle, rightToggle}) {
+
+    const [isToggled, setToggled] = useState(false)
   return (
-    <div>sliderToggle</div>
+    <label >
+        <input type='checkbox' name={name} value={isToggled} style={() => {
+            if(isToggled){
+                return styles.on
+            }else{
+                return styles.off
+            }
+        }}/>
+    </label>
   )
 }
 
+const styles = {
+    container:{
+
+    },
+    on:{
+
+    },
+    off:{
+
+    }
+
+}
 export default sliderToggle
