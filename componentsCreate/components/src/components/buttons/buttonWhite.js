71c0c76d17d1589880c7c41c4fe57836941238a5
A <alyssa@kvasl.com>
2024-04-01 15:29:12 +1300

    Simplified Buttons

---
+++ componentsCreate/components/src/components/buttons/buttonWhite.js
@@ -0,0 +1,30 @@
+import React from 'react'
+ /*
+Button White Background w Black Outline
+Label/ Words on button are children prop
+Action is fed through OnClick prop
+ */
+
+function ButtonWhite({children, onClick}) {
+  return (
+    <button style={styles.button} onClick={onClick}>
+        <p>{children}</p>
+    </button>
+  )
+}
+
+const styles = {
+    button:{
+        backgroundColor: 'white',
+        color: 'black',
+        padding: '0.4rem 2rem',
+        border: '2px, solid, black',
+        borderRadius: '4px',
+        boxShadow: '0px 2px 3px lightgrey',
+
+    }
+}
+
+
+
+export default ButtonWhite
