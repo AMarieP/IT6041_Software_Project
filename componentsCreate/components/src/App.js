d6f57908961ad81d6b420db2d0e43c99b7fe9e60
A <alyssa@kvasl.com>
2024-04-03 17:44:49 +1300

    Testing components, added padding to see test

--- componentsCreate/components/src/App.js
+++ componentsCreate/components/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import SliderToggle from './components/sliderToggle';
+import ButtonWhite from './components/buttons/buttonWhite';
+import ButtonBlack from './components/buttons/buttonBlack';
+import BoxWithDropshadow from './components/boxWithDropshadow';
+import FauxRadio from './components/fauxRadio';
+import TextBox from './components/textBox';
+import TextArea from './components/textArea';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BoxWithDropshadow>
+      <FauxRadio>name of button</FauxRadio>
+      <TextBox name={"textbox small"} thisHeight={'100px'}/>
+      <TextBox name={"textbox large"} thisHeight={'70vh'}/>
+      <TextArea name={"textarea"} thisHeight={'100px'} />
+      <SliderToggle value={'value'} toggleName={'togglename'} toggleId={'thisId'}  >Test Toggle</SliderToggle>
+      </BoxWithDropshadow>
     </div>
   );
 }
