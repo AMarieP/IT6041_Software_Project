47ae38959148ecb4277d3e954599623b6c90d080
A <alyssa@kvasl.com>
2024-04-08 15:20:00 +1200

    imageSChema now subdocument

--- sink/backend/models/matteboardModel.js
+++ sink/backend/models/matteboardModel.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+//Reuse the image schema across models
+const imageSchema = new Schema ({
+    name: String,
+    alt: String, //Alt text for the image
+    url: String, //This is the actual path to the image
+    order: Number, //This sets the order of display the images are in. 
+
+}, { timestamp: true })
+
+
 const matteboardSchema = new Schema ({
     name: {
         type: String,
@@ -21,7 +31,9 @@ const matteboardSchema = new Schema ({
     },
     description: {
         type: String
-    },    
+    },
+    images: [imageSchema],
+
 }, { timestamp: true }) //creates timestamp property when new document is created
 
 module.exports = mongoose.model('Matteboard', matteboardSchema)
