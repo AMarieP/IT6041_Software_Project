8f191e8b8a7a6db25dd088f634c69216aa61a74b
AlexanderJButt <136419342+AlexanderJButt@users.noreply.github.com>
2024-04-11 16:05:28 +1200

    login page
    made the login page look like the figma design and started work on view Stock page.

--- sink/frontend/src/pages/ViewStock.js
+++ sink/frontend/src/pages/ViewStock.js
@@ -1,9 +1,36 @@
-
+import BoxWithDropshadow from "../components/boxWithDropshadow"
+import TextBox from "../components/textBox"
+import FauxRadio from "../components/FauxRadio/fauxRadio"
 
 const ViewStock = () => {
 
 
-    return <h1>View stock</h1>
+    return (
+        <div>
+            <div>
+                <h1>My Stock</h1>
+                <BoxWithDropshadow>
+                    <TextBox/>
+                    <div styles>
+                        <FauxRadio/>
+                        <FauxRadio/>
+                        <FauxRadio/>
+                        <FauxRadio/>
+                        <FauxRadio/>
+                    </div>
+                </BoxWithDropshadow>
+            </div>
+            <div>
+
+            </div>
+
+
+
+
+
+
+        </div>      
+    )
 }
 
 export default ViewStock
