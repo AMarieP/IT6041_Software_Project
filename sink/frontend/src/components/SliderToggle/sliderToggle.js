33c318cd8d8f9c2348ce31685d4ccd24ab9ae97b
A <alyssa@kvasl.com>
2024-04-16 18:35:28 +1200

    toggle styling abd animation finsihed

--- sink/frontend/src/components/SliderToggle/sliderToggle.js
+++ sink/frontend/src/components/SliderToggle/sliderToggle.js
@@ -11,11 +11,16 @@ function SliderToggle({defaultChecked, toggleName, toggleId, children}) {
 
     const [isToggled, setToggle] = useState(false)
     const elementRef = useRef(null)
+    const labelRef = useRef(null)
+    const togRef = useRef(null)
     
     /*Calcs the width of Off/On when toggled, this keeps our element responsive */
     useEffect(() => {
         const elementWidth = elementRef.current.offsetWidth;
+        const labelWidth = labelRef.current.offsetWidth;
+
         document.documentElement.style.setProperty('--element-width', `${elementWidth}px`);
+        document.documentElement.style.setProperty('--label-width', `${labelWidth}px`);
     }, [isToggled]);
 
 
@@ -23,11 +28,13 @@ function SliderToggle({defaultChecked, toggleName, toggleId, children}) {
     /*Sets class and content of On/Off label */
     const onOff = isToggled ? 'on' : 'off'
 
+    const leftRight = isToggled ? 'yes' : 'no'
+
     
   return (
         <label className='sliderContainer'>
             <input type='checkbox' name={toggleName} id={toggleId} defaultChecked={defaultChecked} onClick={() => setToggle(!isToggled)} />
-            <label className='switchLabel' htmlFor={toggleId}>{children}</label>
+            <label className={['switchLabel' + " " + leftRight]} ref={labelRef} htmlFor={toggleId}>{children}</label>
             <label className={onOff} htmlFor={toggleId} ref={elementRef}>{onOff}</label>
         </label>
 
